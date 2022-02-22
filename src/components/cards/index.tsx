9e9335c290cd1731e0cebc268319fbe7fd964449
shehzad002 <shehzad.khan145@yahoo.com>
2022-02-23 02:17:48 +0500

    shehzad

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -8,12 +8,12 @@ const Cards: React.FC = () => {
     const[balanceOf,setBalanceOf] = useState(0);
     const[accounts, setAccounts] = useState<string[]>();
     const[unPaid,setUnPaid] = useState(0);
-	const[user,setUser] = useState(0);
-	const[maxValue , setmaxValue] = useState(0);
+	
+	
     const[value,setValue] = useState('');
     const[busd,setBusd] = useState(0);
 	const[busdBalance,setbusdBalance] = useState(0);
-	const[maxTx,setMaxTx] = useState(0);
+	
 
  useEffect(() => {
     async function getData() {
@@ -34,26 +34,24 @@ const Cards: React.FC = () => {
         const CardValue  = async () => {
             const accounts = await web3.eth.getAccounts();
             console.log(accounts[0])
-          const maxValue = await abi.methods._maxWalletToken().call();
-		  const maxTx = await abi.methods._maxTxAmount().call();
+         
             if(accounts.length >0) {
           const balanceOf = await abi.methods.balanceOf(accounts[0]).call();
 		  const unPaid = await abi.methods.balanceOf(accounts[0]).call();
-		  const user = await abi.methods.isAuthorized(accounts[0]).call();
+		 
 		  const busdBalance = await busdAbi.methods.balanceOf(accounts[0]).call();
           
 		  
 		  setbusdBalance(busdBalance)
 		  
-		  setUser(user)
+		  
           setUnPaid(unPaid)
           setAccounts(accounts)
           setBalanceOf(balanceOf)
           
           
         }
-          setmaxValue(maxValue)
-		  setMaxTx(maxTx)
+          
           
 
           // const CurrentPrice = web3.utils.fromWei(Price, 'ether');
@@ -87,13 +85,7 @@ const Cards: React.FC = () => {
              <br />
          <div className="container">
            <div className="row">
-                  <div className="col-sm-4"> 
-                   <div className="card">
-                       <div className="badge bg-info"><h4>User Status</h4></div>
-                       <h3>{user ? "Whitelisted" : "Banned"}</h3>
-                       <p>Your Status</p>
-                   </div>
-                    </div>
+                
                   <div className="col-sm-4">
                   <div className="card">
                   <div className="badge bg-info">    <h4>Balance</h4></div>
@@ -106,7 +98,7 @@ const Cards: React.FC = () => {
                   <div className="card">
                   <div className="badge bg-info">  <h4>UnPaid Dividends</h4></div>
                        <h3>{unPaid}</h3>
-                       <p>BUSD</p>
+                       <p>EBIRD</p>
                    </div>
                   </div>
 
@@ -123,22 +115,7 @@ const Cards: React.FC = () => {
                   </div>
 				  
 				  
-				    <div className="col-sm-4">
-                  <div className="card">
-                  <div className="badge bg-info">  <h4>Max Wallet Balance</h4></div>
-                       <h3>{Number(maxValue / 1e9).toFixed(2)}</h3>
-                       <p>EBIRD</p>
-                   </div>
-                  </div>
-				  
-				  
-				   <div className="col-sm-4">
-                  <div className="card">
-                  <div className="badge bg-info">  <h4>Max Tx Amount</h4></div>
-                       <h3>{Number(maxTx / 1e9).toFixed(2)}</h3>
-                       <p>EBIRD</p>
-                   </div>
-                  </div>
+				 
 			
 
 
